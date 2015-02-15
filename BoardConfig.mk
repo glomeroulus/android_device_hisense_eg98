78a25414f752d0ee24eeec776c70bd612ef22b10
glomeroulus <you@example.com>
2015-02-16 00:58:39 +0700

    raw

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,9 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := EG98
 
-BOARD_KERNEL_CMDLINE := 
-BOARD_KERNEL_BASE := 0x
-BOARD_KERNEL_PAGESIZE := 
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom loglevel=1 vmalloc=200M
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_KERNEL_PAGESIZE := 4096
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
@@ -31,4 +31,14 @@ TARGET_PREBUILT_KERNEL := device/Hisense/EG98/kernel
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_UMS_LUNFILE := "/sys/devices/platform/mt_usb/gadget/lun0/file"
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+TARGET_RECOVERY_FSTAB := device/hisense/eg98/fstab.qcom
+TARGET_PREBUILT_RECOVERY_KERNEL := device/hisense/eg98/recovery-kernel
+
+# Files needed for recovery image
+PRODUCT_COPY_FILES += \
+device/lenovo/armani_row/recovery/sbin/rmt_storage_recovery:/recovery/root/sbin/rmt_storage_recovery \
+device/lenovo/armani_row/recovery/sbin/charger:/recovery/root/sbin/charger \
+device/lenovo/armani_row/recovery/sbin/charge.sh:/recovery/root/sbin/charge.sh \
+device/lenovo/armani_row/recovery/umountusb.sh:root/umountusb.sh
 
